fc72fd7a2d4d7bb33ba347a9ad29e77815f0b57a
Pierce Corcoran <pierce@plasticcow.com>
2019-04-19 15:50:00 -0700

    improve prime calculation algorithm

--- asm/hello_world.asm
+++ asm/hello_world.asm
@@ -1,11 +1,17 @@
 current = 0
-testi = 1
+half_current = 1
+testi = 2
+endi = 3
 
 move32 '2 -> >0
-move8 '1 -> %current
+move8 '4 -> %endi
+move8 '2 -> %current
 
 next_prime:
     calc.u add (%current, '1) -> %current
+    calc.u add (%current, '1) -> %half_current
+    calc.u div (%half_current, '2) -> %half_current
+
     move8 '0 -> %testi
 
     print_str
@@ -15,7 +21,7 @@ next_prime:
     #str ": \0"
 
 test_loop:
-    jumpif.u eqz ( >%testi ) -> 'success
+    ;jumpif.u eqz ( >%testi ) -> 'success
 
     jumpif.u eqz ( %testi ) -> '.after_comma
     print_str
@@ -24,6 +30,7 @@ test_loop:
     print.u >%testi
 
     jumpif.u eqz calc mod (%current, >%testi) -> 'failure
+    jumpif.u ( >%testi, geq, %half_current ) -> 'success
     calc.u add (%testi, '4) -> %testi
     jump 'test_loop
 
@@ -36,6 +43,7 @@ success:
     #str " - prime\0"
     print_nl
 
-    move32 %current -> >%testi
+    move32 %current -> >%endi
+    calc.u add (%endi, '4) -> %endi
     jump 'next_prime
 halt
